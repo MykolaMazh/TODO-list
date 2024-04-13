26dbd5d77a64146f31770405902b90cd43503dd2
MykolaMazhula <mykm3ua@gmail.com>
2024-04-13 20:43:01 +0200

    Add Task and Tag models

--- todo_list/app_todo_list/models.py
+++ todo_list/app_todo_list/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(blank=True, null=True)
+    tags = models.ManyToManyField(related_name='tasks')
+    is_done = models.BooleanField()
+    # atetime, when a task was created
+# optional deadline datetime if a task should be done until some datetime
+# the boolean field that marks if the task is done or not
+# tags that are relevant for this task
+# Tag - a tag symbolizes the theme of the task and consists only of a name.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=75)

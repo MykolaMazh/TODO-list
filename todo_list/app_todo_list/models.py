221c089789623cf7008327858e2b788f8c8c936b
MykolaMazhula <mykm3ua@gmail.com>
2024-04-13 21:50:12 +0200

    Correct tags field of the model Task Add str method to Task and Tag models

--- todo_list/app_todo_list/models.py
+++ todo_list/app_todo_list/models.py
@@ -5,13 +5,15 @@ class Task(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateField(blank=True, null=True)
-    tags = models.ManyToManyField(related_name='tasks')
+    tags = models.ManyToManyField("Tag", related_name='tasks')
     is_done = models.BooleanField()
-    # atetime, when a task was created
-# optional deadline datetime if a task should be done until some datetime
-# the boolean field that marks if the task is done or not
-# tags that are relevant for this task
-# Tag - a tag symbolizes the theme of the task and consists only of a name.
+
+    def __str__(self):
+        return self.description
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.name

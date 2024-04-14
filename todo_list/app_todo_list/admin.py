4491512b4032ffe2fca7d254a8f458cdfaa711d0
MykolaMazhula <mykm3ua@gmail.com>
2024-04-14 08:09:14 +0200

    Edit Task model display in admin.py

--- todo_list/app_todo_list/admin.py
+++ todo_list/app_todo_list/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Tag, Task
 
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("description", "deadline", "is_done")
+    list_editable = ("is_done",)
+
+
 admin.site.register(Tag)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)

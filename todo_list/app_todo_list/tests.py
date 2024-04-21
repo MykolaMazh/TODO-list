0609176dfb9b1e6c6bd3ad7e5e472a88b64b5cfb
MykolaMazhula <mykm3ua@gmail.com>
2024-04-21 22:32:53 +0200

    Change tests.py to test post method of TaskupdateView

--- todo_list/app_todo_list/tests.py
+++ todo_list/app_todo_list/tests.py
@@ -9,23 +9,23 @@ class TaskUpdateViewTest(TestCase):
     def setUp(self):
         Task.objects.create(description="New Test Task")
 
-    def test_get_to_update_status_only(self):
-        self.client.get(
+    def test_post_to_update_status_only(self):
+        self.client.post(
             reverse(
-                "todo_list:task-update", kwargs={"pk": 1, "change_status": 1}
-            )
+                "todo_list:task-update", kwargs={"pk": 1}
+            ), data={"change-task-status": "change"}
         )
         self.assertEqual(Task.objects.get(pk=1).is_done, True)
-        self.client.get(
+        self.client.post(
             reverse(
-                "todo_list:task-update", kwargs={"pk": 1, "change_status": 1}
-            )
+                "todo_list:task-update", kwargs={"pk": 1}
+            ), data={"change-task-status": "change again"}
         )
         self.assertEqual(Task.objects.get(pk=1).is_done, False)
 
-    def test_get_default(self):
+    def test_post_default(self):
         response = self.client.get(
-            reverse("todo_list:task-update", args=[1, 0])
+            reverse("todo_list:task-update", args=[1])
         )
         self.assertEqual(Task.objects.get(pk=1).is_done, False)
         self.assertEqual(response.status_code, 200)
